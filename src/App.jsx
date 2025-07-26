8e592d156c1338b4dad086d0eaa6daa1b75472ed
popcorn3-com-dev <shos3104@gmail.com>
2025-07-26 18:06:05 +0900

    sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
+import { CssModules } from './components/CssModules';
 import { InlineStyle } from './components/InlineStyle';
 
 function App() {
   return (
     <div>
       <InlineStyle />
-
+      <CssModules />
     </div>
   );
 }
---
+++ src/components/CssModules.jsx
@@ -0,0 +1,11 @@
+import classes from './CssModules.module.scss';
+
+export const CssModules = () => {
+    return (
+        <div className={classes.container}>
+            <p>--CssModules--</p>
+            <button>FIGHT!!</button>
+        </div>
+    )
+
+}
