cd0f15b64b05010f58b16be558a0d7d184573902
popcorn3-com-dev <shos3104@gmail.com>
2025-08-17 16:44:01 +0900

    stateを渡すページ遷移

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,11 +1,14 @@
-import { Link, Outlet } from "react-router";
+import { Link, Outlet } from "react-router-dom";
+
 
 export const Page1 = () => {
+    const arr = [...Array(100).keys()];
+
     return (
         <div>
             <h1>Page1です</h1>
             <nav>
-                <Link to='detailA'>DetailA</Link><br />
+                <Link to='detailA' state={arr}>DetailA</Link><br />
                 <Link to='detailB'>DetailB</Link><br />
             </nav>
             <Outlet />
--- src/components/Page1DetailA.jsx
+++ src/components/Page1DetailA.jsx
@@ -1,4 +1,9 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+    const location = useLocation();
+    console.log(location);
+
     return (
         <div>
             <h2>Page1DetailA</h2>
